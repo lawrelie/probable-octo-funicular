58ad9f50e89b2a06f3d00cd269fd21e149a3a93a
lawrelie <85341666+lawrelie@users.noreply.github.com>
2021-06-26 06:02:03 +0900

    修正：名前空間エイリアス「lwpta」

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,12 @@
 <?php
-use Lawrelie\WordPress\TermAuthor as lwta;
+use Lawrelie\WordPress\TermAuthor as lwpta;
 require_once __DIR__ . '/lawrelie-term-author.php';
 $terms = get_terms();
 if (is_wp_error($terms)) {
     return;
 }
 foreach ($terms as $term) {
-    foreach (lwta\META_KEYS as $key) {
-        delete_term_meta($term->term_id, lwta\metaKey($key));
+    foreach (lwpta\META_KEYS as $key) {
+        delete_term_meta($term->term_id, lwpta\metaKey($key));
     }
 }
